d3559cebb746a6d3507891c9ec8d524951701bd5
Isaac Acosta López <iscal88@gmail.com>
2014-10-14 20:52:36 +0200

    subiendo cambios para redesplegar

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-var path = "http://shortnr-iacosta.rhcloud.com";
+var path = "http://shortnr.es";
 
 $(document).ready(function() {
 
--- shortnr.js
+++ shortnr.js
@@ -1,23 +1,29 @@
 var express = require("express"),
-	mongoose = require("mongoose"),
-	bodyParser = require("body-parser"),
-	cors = require('cors'),
-	app = express();
+    mongoose = require("mongoose"),
+    bodyParser = require("body-parser"),
+    cors = require('cors'),
+    app = express();
 
-var debugMode = true;
+var debugMode = false;
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/', express.static(__dirname + '/public'));
 
-var options = {
-  db: { native_parser: true },
-  server: { poolSize: 5 },
-  user: 'admin',
-  pass: 'mZWE6EN-lWWY'
-}
-mongoose.connect('mongodb://localhost/shortnr', options);
-
 require('./routes')(app, debugMode);
+
+var connection_string = '127.0.0.1:27017/shortnr';
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+        connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
+mongoose.connect('mongodb://' + connection_string, function(err, db) {
+        if (err) console.log("ERROR de conexión a la BD.");
+        else console.log("Conectado correctamente a la base de datos");
+});
 
 var port =  process.env.OPENSHIFT_NODEJS_PORT || 8080;   // Port 8080 if you run locally
 var address =  process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"; // Listening to localhost if you run locally
